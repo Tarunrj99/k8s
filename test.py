bb69ae8e155982fe74c08c9c3ea97b06a933b812
Tarunrj99 <86702050+Tarunrj99@users.noreply.github.com>
2024-07-12 04:01:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--input_file", required=True, help="Input file")
+    parser.add_argument("-o", "--output_file", required=True, help="Output file")
+    args = parser.parse_args()
+
+    with open(args.input_file, "r") as input_file:
+        content = input_file.read()
+
+    with open(args.output_file, "w") as output_file:
+        output_file.write(content)
+
+if __name__ == "__main__":
+    main()
